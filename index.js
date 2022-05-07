8dd57edb592b08609e36575565e6307ea7fda0bb
Aditya Singh <91184149+Aditya-devp@users.noreply.github.com>
2022-05-07 17:33:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,31 +16,31 @@ document.addEventListener ("keydown", function(){
 function makeSound(key){
     switch (key) {
         case "w":
-          var tom1 = new Audio("sounds/tom-1.mp3");
+          var tom1 = new Audio("tom-1.mp3");
           tom1.play();
           break;
         case "a":
-          var tom2 = new Audio("sounds/tom-2.mp3");
+          var tom2 = new Audio("tom-2.mp3");
           tom2.play();
           break;
         case "s":
-          var tom3 = new Audio("sounds/tom-3.mp3");
+          var tom3 = new Audio("tom-3.mp3");
           tom3.play();
           break;
         case "d":
-          var tom4 = new Audio("sounds/tom-4.mp3");
+          var tom4 = new Audio("tom-4.mp3");
           tom4.play();
   
         case "j":
-          var snare = new Audio("sounds/snare.mp3");
+          var snare = new Audio("snare.mp3");
           snare.play();
           break;
         case "k":
-          var crash = new Audio("sounds/crash.mp3");
+          var crash = new Audio("crash.mp3");
           crash.play();
           break;
         case "l":
-          var kick = new Audio("sounds/kick-bass.mp3");
+          var kick = new Audio("kick-bass.mp3");
           kick.play();
           break;
         default:
